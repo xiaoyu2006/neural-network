68cede63cce95885afb446fc1221fc2653f91ab8
xiaoyu2006 <1783250036@qq.com>
2019-02-05 22:27:12 +0800

    A small bug

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from network import *
 
-n=NeuralNetwork(784,500,10,0.1)
+n=NeuralNetwork(784,25,10,0.3)
 
 f=open('mnist-data/mnist_train.csv')
 data_list=f.readlines()
 f.close()
 
 # TRAIN
-epochs=5
+epochs=2
 for _ in range(epochs):
     print "The %dth epochs."%(_+1)
     count=0
@@ -16,19 +16,19 @@ for _ in range(epochs):
         if count%1000==0:
             print "\tLearning the %dth data..."%count
         all_values=record.strip().split(',')
-        inputs=np.asfarray(all_values[1:])/255.0*0.99+0.01
-        targets=np.zeros(10)+0.01
+        inputs=numpy.asfarray(all_values[1:])/255.0*0.99+0.01
+        targets=numpy.zeros(10)+0.01
         targets[int(all_values[0])]=0.99
         n.train(inputs,targets)
 
 del record,data_list,all_values,inputs,targets,count
 
 # SAVE
-import pickle
-f=open('MNIST.neuralnetwork','w')
-pickle.dump(n,f)
-f.close()
-del pickle
+#import pickle
+#f=open('MNIST.neuralnetwork','w')
+#pickle.dump(n,f)
+#f.close()
+#del pickle
 
 # TEST
 right=0
@@ -40,9 +40,9 @@ del f
 for record in data_list:
     all_values=record.strip().split(',')
     correct_label=int(all_values[0])
-    inputs=np.asfarray(all_values[1:])/255.0*0.99+0.01
+    inputs=numpy.asfarray(all_values[1:])/255.0*0.99+0.01
     outputs=n.query(inputs)
-    label=np.argmax(outputs)
+    label=numpy.argmax(outputs)
     if label==correct_label:
         right+=1
     s+=1
--- network.py
+++ network.py
@@ -1,41 +1,32 @@
-import numpy as np
+import numpy
+import scipy.special
 
-def sigmoid(input):
-    tmp=1+np.exp(input)
-    return 1/tmp
-
-class NeuralNetwork(object):
-    def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
-        self.innodes=inputnodes
-        self.hnodes=hiddennodes
-        self.onodes=outputnodes
-        self.wih=np.random.normal(0.0,pow(hiddennodes,0.5),(hiddennodes,inputnodes))
-        self.who=np.random.normal(0.0,pow(outputnodes,0.5),(outputnodes,hiddennodes))
-        self.lr=learningrate
-        self.activation_func=sigmoid
+class NeuralNetwork:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+        self.lr = learningrate
+        self.activation_function = lambda x: scipy.special.expit(x)
     
-    def train(self,inputs_list,targets_list):
-        inputs=np.array(inputs_list,ndmin=2).T
-        targets=np.array(targets_list,ndmin=2).T
-        hidden_inputs=np.dot(self.wih,inputs)
-        hidden_outputs=self.activation_func(hidden_inputs)
-        final_inputs=np.dot(self.who,hidden_outputs)
-        final_outputs=self.activation_func(final_inputs)
-        errors=targets-final_outputs
-        hidden_errors=np.dot(self.who.T,errors)
-        self.who+=self.lr*np.dot(
-            (errors*final_outputs*(1.0-final_outputs)),
-            np.transpose(hidden_outputs)
-        )
-        self.wih+=self.lr*np.dot(
-            (hidden_errors*hidden_outputs*(1.0-hidden_outputs)),
-            np.transpose(inputs)
-        )
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
     
-    def query(self,input_list):
-        inputs=np.array(input_list,ndmin=2).T
-        hidden_inputs=np.dot(self.wih,inputs)
-        hidden_outputs=self.activation_func(hidden_inputs)
-        final_inputs=np.dot(self.who,hidden_outputs)
-        final_outputs=self.activation_func(final_inputs)
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
         return final_outputs
